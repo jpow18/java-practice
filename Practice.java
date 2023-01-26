7a4f6d34573cc1f4e4c232d06a48b1fdc0c080fc
James Pow <jbpow18@icloud.com>
2023-01-26 07:47:37 -0500

    First commit.

---
+++ Practice.java
@@ -0,0 +1,14 @@
+
+/**
+ * Practice in Java, inspired by course on FCC
+ *
+ * @author James Pow
+ */
+public class Practice
+{
+    public static void main(String[] args) {
+        String name = "James Pow";
+        name = name.toUpperCase();
+        System.out.println(name);
+    }
+}
