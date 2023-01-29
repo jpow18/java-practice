ea5f5a24d30bb09c96d20b1ec1e2b9e8b5f46447
James Pow <jbpow18@icloud.com>
2023-01-29 09:05:54 -0500

    First commit.

---
+++ ComputeAreaOfCircle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class ComputeAreaOfCircle
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double radius = 0;
+        // catch the error produced if user doesn't enter a double
+        while (true) {
+            try {
+                // user prompt
+                System.out.print("Enter a number for radius: ");
+                radius = input.nextDouble();
+                break;
+            } catch (InputMismatchException error) {
+                System.out.println("Invalid input. Please enter a double.");
+                input.nextLine();
+            }
+        }
+        // calculate area
+        double area = radius * radius * 3.14159;
+        // display result
+        System.out.printf("The area for a circle of radius %.1f is %.5f.\n",radius,area);
+    }
+}
