cd7fee5b9a86746ed99209b8a143fb0ba0f68497
James Pow <jbpow18@icloud.com>
2023-02-02 18:52:42 -0500

    Use extra scanner.nextLine() to clean up newlines left in buffer from nextInt(), etc...

--- Practice.java
+++ Practice.java
@@ -10,10 +10,18 @@ public class Practice
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("What is your name?");
+        System.out.print("What is your name? ");
         String name = scanner.nextLine();
         
-        System.out.println(name);
+        System.out.printf("Hello %s. How old are you? ", name);
+        int age = scanner.nextInt();
+        
+        // clean up input buffer
+        scanner.nextLine();
+        System.out.printf("%d is an age. What's a programming language? ", age);
+        int num = scanner.nextInt();
+        
+        System.out.printf("%d is a number, stupid.", num); 
         
         scanner.close();
     }
