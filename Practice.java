cdc1ea1127e057b254a1c2560518a41311393b0b
James Pow <jbpow18@icloud.com>
2023-02-03 09:03:10 -0500

    java.util.Arrays Arrays.binarySearch()

--- Practice.java
+++ Practice.java
@@ -11,9 +11,14 @@ public class Practice
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        char vowels[] = {'a', '3', 'a', 'v'};
-        Arrays.sort(vowels);
+        char vowels[] = {'a', 'z', 'a', 'v', '1', 'a', 'f', '4', 'b', '-','5'};
         System.out.println((Arrays.toString(vowels)));
+
+        Arrays.sort(vowels);
+        int foundItemIndex = Arrays.binarySearch(vowels, '5');
+        
+        System.out.println((Arrays.toString(vowels)));
+        System.out.println(foundItemIndex);
         scanner.close();
     }
 
