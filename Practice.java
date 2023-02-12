1e1ab977c64575e4f836403c9e7c57b0fb12bdd5
James Pow <jbpow18@icloud.com>
2023-02-12 16:29:42 -0500

    HashMap practice. .forEach(), .replace()

--- Practice.java
+++ Practice.java
@@ -14,12 +14,26 @@ public class Practice
         examScores.put("English", 52);
         examScores.put("History", 51);
         
+        
+        examScores.forEach((subject, score) -> {
+            examScores.replace(subject, score - 10);
+            //System.out.println(subject + " - " + score);
+        });
         //examScores.putIfAbsent("Math", 70);
         
         //examScores.replace("Math", 2); // Replace value associated with key
         
-        System.out.println("Your Physics score is: " + 
-        examScores.getOrDefault("Physics", -1));
+        //examScores.remove("History"); // Clears key/value
+        System.out.println(examScores);
+        //examScores.clear(); // Clears contents
+        
+        //System.out.println(examScores.containsKey("Math")); // returns bool
+        //System.out.println(examScores.containsValue(Integer.valueOf(52))); // returns bool
+
+        //System.out.println(examScores.size());
+        //System.out.println(examScores.isEmpty());
+        //System.out.println("Your Physics score is: " + 
+        //examScores.getOrDefault("Physics", -1));
     }
 
 }
