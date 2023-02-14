802f10839f2f8206c381854b44a650d70db0314a
James Pow <jbpow18@icloud.com>
2023-02-14 17:19:41 -0500

    OOP. Constructor

--- Practice.java
+++ Practice.java
@@ -8,13 +8,20 @@ import java.time.LocalDate;
 public class Practice
 {
     public static void main(String[] args) {
-        User youngerUser = new User();
+        User user = new User("James Pow", "1995-01-31");
         
-        youngerUser.name = "James Pow";
-        youngerUser.birthday = LocalDate.parse("1995-01-31");
+        Book book = new Book();
         
-        System.out.printf("%s was born back in %s and he is now %d years old.",
-        youngerUser.name, youngerUser.birthday, youngerUser.age());
+        book.title = "Harry Potter";
+        book.author = "Jk Rowling";
+        
+        user.borrow(book);
+        
+        System.out.printf("%s was born back in %s and he is now %d years old. \n",
+        user.name, user.birthday, user.age());
+        
+        System.out.printf("%s has borrowed these books: %s \n",
+        user.name, user.books.toString());
     }
 
 }
