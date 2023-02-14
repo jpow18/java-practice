f31e7f89e5d28afc4f0d864afa5772148b22b098
James Pow <jbpow18@icloud.com>
2023-02-14 17:56:32 -0500

    Practice inheritance.

--- Practice.java
+++ Practice.java
@@ -8,17 +8,10 @@ import java.time.LocalDate;
 public class Practice
 {
     public static void main(String[] args) {
-        User user = new User("James Pow", "1995-01-31");
-        
-        Book book = new Book("Harry Potter", "Jk Rowling");
-        
-        user.borrow(book);
-        
-        System.out.printf("%s was born back in %s and he is now %d years old. \n",
-        user.getName(), user.getBirthday(), user.age());
-        
-        System.out.printf("%s has borrowed these books: %s \n",
-        user.getName(), user.books.toString());
+        Book hp = new Book("Harry Potter", "Jk Rowling", 327);
+        AudioBook drac = new AudioBook("Dracula", "Bram Stoker", 30000);
+        Ebook jeeves = new Ebook("Carry on Jeeves", "P.G. Wodehouse", 288, "PDF");
+        System.out.print(jeeves.toString());
     }
 
 }
