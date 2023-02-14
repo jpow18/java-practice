659096a571c3a23a1dfe61d4a559b410fd3d671c
James Pow <jbpow18@icloud.com>
2023-02-14 17:36:06 -0500

    Added getters and setters. Constructor to Book class

--- Practice.java
+++ Practice.java
@@ -10,18 +10,15 @@ public class Practice
     public static void main(String[] args) {
         User user = new User("James Pow", "1995-01-31");
         
-        Book book = new Book();
-        
-        book.title = "Harry Potter";
-        book.author = "Jk Rowling";
+        Book book = new Book("Harry Potter", "Jk Rowling");
         
         user.borrow(book);
         
         System.out.printf("%s was born back in %s and he is now %d years old. \n",
-        user.name, user.birthday, user.age());
+        user.getName(), user.getBirthday(), user.age());
         
         System.out.printf("%s has borrowed these books: %s \n",
-        user.name, user.books.toString());
+        user.getName(), user.books.toString());
     }
 
 }
