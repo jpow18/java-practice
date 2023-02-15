92aa93b8496f0095094fd900592107957d00f03d
James Pow <jbpow18@icloud.com>
2023-02-15 14:56:04 -0500

    Practice with the trig methods of the Math object.

---
+++ MathDemo.java
@@ -0,0 +1,8 @@
+public class MathDemo
+{
+    public static void main(String[] args) {
+        System.out.println(Math.cos(Math.PI / 6));
+        System.out.println(Math.toRadians(30));
+        System.out.println(Math.toDegrees(0.52));
+    }
+}
